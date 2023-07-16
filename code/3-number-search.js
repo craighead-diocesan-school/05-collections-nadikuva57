1be5c0d504bcd7bb88d1634b63a6f14972fde3a7
Varuni <nadikuva57@craighead.school.nz>
2023-07-17 06:43:19 +1200

    update

--- code/3-number-search.js
+++ code/3-number-search.js
@@ -7,17 +7,26 @@ let numberArray = [
   100,
   1000,
   10000,
-  1234567890
+  1234567890,
+  46,
+  76
+
 ]
 
 function searchNumbers() {
-  let searchTarget = 1
+  let searchTarget = prompt("enter a number to search for:")
   let searchResult = false
 
   for (let currentNumber of numberArray) {
-    if (currentNumber == searchTarget) {
+    if (currentNumber === Number(searchTarget)) {
       searchResult = true
     }
+  }
+
+  if(searchResult){
+    alert(searchTarget + 'was found')
+  }else{
+    alert(searchTarget + 'was not found')
   }
 
   alert(searchTarget + ' found: ' + searchResult)
--- code/4-subject-details.js
+++ code/4-subject-details.js
@@ -6,14 +6,20 @@ let subject = {
   name: 'DigiTech',
   room: 'Room 9',
   students: 150,
+  teacher: 'Random middle-aged man'
 }
 
 function showSubject() {
-  alert(subject.name)
+  alert(subject.name + subject.room)
 }
 
 function changeSubject() {
   let newName = 'Typing Skills'
 
   subject.name = newName
+}
+
+function changeTeacher() {
+  let newTeacher = prompt('Enter the name if the new teacher')
+  subject.teacher = newTeacher
 }
